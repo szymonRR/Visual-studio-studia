0f753300a61603b29a7be8e1bc0adeed2bae5157
szymonRR <szymon32radom@gmail.com>
2019-07-01 13:26:48 +0200

    Add files via upload

---
+++ RozpoznawanieLiter v1.3/RozpoznawanieLiter/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace RozpoznawanieLiter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(569, 316);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Litera G:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(569, 365);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Litera R:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(569, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Litera O:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(622, 362);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(152, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(622, 313);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(152, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(622, 267);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(639, 177);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 59);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Czyszczenie obrazu";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(639, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 59);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Rozpoznanie litery";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(639, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 59);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Uczenie Sieci";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(530, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Najpierw uczenie -->";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(639, 430);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 56);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "dodanie wzorca";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 541);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Rozpoznawanie liter O, G, R";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ RozpoznawanieLiter v1.3/RozpoznawanieLiter/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RozpoznawanieLiter
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        private Pen rys = new Pen(Color.Black);
+        private Brush brush = new SolidBrush(Color.Black);
+
+        private int ileEpok;
+        private double uczenie;
+        private double bladMax;
+        private int[,] wzor = new int[100, 64];
+
+        private int numerWzorca = 0;
+        private Point p = Point.Empty;
+
+        private int dlugoscO;
+        private int dlugoscG;
+        private int dlugoscR;
+
+        double[,] w = new double[3, 64];
+        double[] y = new double[3];
+
+        private Boolean klik = true;
+        private Boolean maluj = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(512, 512);  //Tworzenie pustej bitmapy w PictureBoxie
+            g = Graphics.FromImage(pictureBox1.Image);
+
+        }
+
+        private void Wagi() //Obliczenie wag dla wspólczynnika odpowiadającego każdej literze
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < 3; ++i)
+                for (int j = 0; j < 64; ++j)
+                    w[i, j] = (0.1 - 0.01) * r.NextDouble() + 0.01;
+        }
+
+        private void Skalowanie() //skalowanie obrazka żeby zajmowal cały PictureBox
+        {
+            int left = 512;
+            int right = 0;
+            int up = 512;
+            int down = 0;
+
+            Bitmap bitmap = new Bitmap(pictureBox1.Image);
+
+            for (int i = 0; i < 512; ++i)
+                for (int j = 0; j < 512; ++j)
+                {
+                    Color kolor = bitmap.GetPixel(i, j);
+                    if (kolor.R == 0 && kolor.G == 0 && kolor.B == 0)
+                    {
+                        if (left > i) left = i;
+                        if (right < i) right = i;
+                        if (up > j) up = j;
+                        if (down < j) down = j;
+                    }
+                }
+
+            Bitmap bitmapPainted = new Bitmap(right - left + 1, down - up + 1);
+
+            for (int i = 0; i <= right - left; ++i)
+            {
+                for (int j = 0; j <= down - up; ++j)
+                {
+                    bitmapPainted.SetPixel(i, j, bitmap.GetPixel(left + i, up + j));
+                }
+            }
+
+            Graphics g3 = Graphics.FromImage(pictureBox1.Image);
+
+            g3.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;  //rysowanie przeskalowanej bitmapy
+            g3.DrawImage(bitmapPainted, 0, 0, 512, 512);
+            pictureBox1.Refresh();
+        }
+
+        private double ObliczWartoscNeuronu(int numPattern, int numNeuron) //przeliczenie wartości neuronów
+        {
+            double z0 = 0;
+            double z1 = 0;
+            double z2 = 0;
+
+            if (numPattern <= dlugoscO) z0 = 1;
+            else if (numPattern <= dlugoscO + dlugoscG) z1 = 1;
+            else if (numPattern <= dlugoscO + dlugoscG + dlugoscR) z2 = 1;
+
+
+            if (numNeuron == 0) return z0 - y[0];
+            else if (numNeuron == 1) return z1 - y[1];
+            else return z2 - y[2];
+        }
+
+        private void ZmianaWag(int numPattern, int epoch)  //zmiana wartości wag (proces uczenia)
+        {
+            for (int i = 0; i < 3; ++i)
+                for (int j = 0; j < 64; ++j)
+                {
+                    double deltaW = uczenie * ObliczWartoscNeuronu(numPattern, i) * ((1 - y[i]) * y[i]) * wzor[numPattern, j];
+                    w[i, j] += deltaW;
+                }
+        }
+
+        private void LoadWzor()  //załadowanie wzorcowych obrazów w formacie jpg z folderu ObrazyWzorcowe
+        {
+            string[] literaO = Directory.GetFiles(@"obrazyWzorcowe\O\");
+            string[] literaG = Directory.GetFiles(@"obrazyWzorcowe\G\");
+            string[] literaR = Directory.GetFiles(@"obrazyWzorcowe\R\");
+
+            dlugoscO = literaO.Length;
+            dlugoscG = literaG.Length;
+            dlugoscR = literaR.Length;
+
+            for (int f = 0; f < dlugoscO; ++f)
+            {
+                ++numerWzorca;
+
+                ReadImage(literaO[f]);
+
+                for (int i = 0; i < 64; i++)
+                    wzor[numerWzorca, i] = wzor[0, i];
+            }
+
+            for (int f = 0; f < dlugoscG; ++f)
+            {
+                ++numerWzorca;
+
+                ReadImage(literaG[f]);
+
+                for (int i = 0; i < 64; i++)
+                    wzor[numerWzorca, i] = wzor[0, i];
+            }
+
+            for (int f = 0; f < dlugoscR; ++f)
+            {
+                ++numerWzorca;
+
+                ReadImage(literaR[f]);
+
+                for (int i = 0; i < 64; i++)
+                    wzor[numerWzorca, i] = wzor[0, i];
+            }
+        }
+
+        private void ReadImage(string imgPath)  //sprawdzanie czy dany fragment wzorcowego obrazka jest zamalowany
+        {
+            Bitmap bitmap = new Bitmap(Image.FromFile(@imgPath));
+
+            for (int i = 0; i < 64; i++)
+                wzor[0, i] = 0;
+
+            for (int i = 0; i < 512; ++i)
+            {
+                for (int j = 0; j < 512; ++j)
+                {
+                    Color kolor = bitmap.GetPixel(i, j);
+                    if (kolor.R != 255 || kolor.G != 255 || kolor.B != 255)
+                        wzor[0, i / 64 + j / 64 * 8] = 1;
+                }
+            }
+        }
+
+        private void TworzenieCiagu()  //budowanie ciągu '0' '1' zależnie od zamalowania obrazka we wzorcu, ciąg wzorcowy
+        {
+            Bitmap bitmap = new Bitmap(pictureBox1.Image);
+
+            for (int i = 0; i < 64; i++)
+                wzor[0, i] = 0;
+
+            for (int i = 0; i < 512; ++i)
+            {
+                for (int j = 0; j < 512; ++j)
+                {
+                    Color kolor = bitmap.GetPixel(i, j);
+                    if (kolor.R != 255 || kolor.G != 255 || kolor.B != 255)
+                        wzor[0, i / 64 + j / 64 * 8] = 1;
+                }
+            }
+        }
+
+        private void ComputeOutputs(int numerWzorca)  //obliczanie wyjścia z neuronu na podstawie wag i wzorca
+        {
+            for (int i = 0; i < y.Length; ++i)
+            {
+                y[i] = 0;
+                for (int j = 0; j < 64; ++j) y[i] += wzor[numerWzorca, j] * w[i, j];
+                y[i] = Aktywacji(y[i]);
+            }
+        }
+
+        private double ComputeErrorNetwork()
+        {
+            double sumErrorsInNetwork = 0;
+
+            for (int i = 1; i <= numerWzorca; ++i)
+            {
+                ComputeOutputs(i);
+                double sumErrorsInPattern = 0;
+                for (int j = 0; j < 3; ++j) sumErrorsInPattern += Math.Abs(ObliczWartoscNeuronu(i, j));
+                sumErrorsInNetwork += sumErrorsInPattern / 3;
+            }
+
+            return sumErrorsInNetwork / numerWzorca;
+        }
+
+        private bool IsBadErrorNetwork(int numPattern)
+        {
+            double z0 = 0;
+            double z1 = 0;
+            double z2 = 0;
+
+            double errorZ0;
+            double errorZ1;
+            double errorZ2;
+
+            if (numPattern <= dlugoscO) z0 = 1;
+            else if (numPattern <= dlugoscO + dlugoscG) z1 = 1;
+            else if (numPattern <= dlugoscO + dlugoscG + dlugoscR) z2 = 1;
+
+            errorZ0 = Math.Abs(z0 - y[0]);
+            errorZ1 = Math.Abs(z1 - y[1]);
+            errorZ2 = Math.Abs(z2 - y[2]);
+
+            if (errorZ0 > bladMax || errorZ1 > bladMax || errorZ2 > bladMax) return true;
+            return false;
+        }
+
+        private double Aktywacji(double y)  //Funkcja aktywacji neuronu (sigmoidalna)
+        {
+            double e = 2.72;
+            double B = 1;
+            return 1.0 / (1.0 + Math.Pow(e, -B * y));
+        }
+
+        public void a()  //uczenie
+        {
+            ileEpok = 1000;
+            uczenie = 0.1;
+            bladMax = 0.01;
+            LoadWzor();
+            Wagi();
+
+            int epoka = 0;
+
+            while (epoka < ileEpok) //kolejne epoki uczenia
+            {
+                
+                bool badError = false;
+                for (int i = 1; i <= numerWzorca; ++i)
+                {
+                    ComputeOutputs(i); //obliczenie wyjścia neuronu
+
+                    if (IsBadErrorNetwork(i) == true)
+                    {
+                        ZmianaWag(i, epoka); //zmiana wag i powrót na poczatek (sprzeżenie zwrotne)
+                        badError = true;
+                    }
+                }
+                if (badError == false)
+                {
+                    break;
+                }
+                
+                ++epoka;
+                if (epoka >= 1000) { label4.Invoke(new Action(delegate() { label4.Text = "Już nauczone"; })); }
+            }
+            
+            MessageBox.Show("Sieć nauczona", "Nauczanie zakończone");
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            label4.Text = "Prosze czekać";
+            System.Threading.Thread newThread =
+            new System.Threading.Thread(a);
+            newThread.Start();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e) //rozpoznanie konkretnej litery i przypisanie do TextBoxów stopnia podobieństwa do danej litery
+        {
+            Skalowanie();
+            TworzenieCiagu();
+            ComputeOutputs(0);
+            for (int i = 0; i < 3; i++ )
+            {
+                y[i] = y[i] * 100;
+                y[i] = Math.Floor(y[i]);
+                y[i] = y[i] / 100;
+            }
+                textBox1.Text = Convert.ToString(y[0]);
+            textBox2.Text = Convert.ToString(y[1]);
+            textBox3.Text = Convert.ToString(y[2]);
+            
+            if ((y[0] >= y[1]) && (y[0] >= y[2])) MessageBox.Show("Wybrana litera to O", "Wynik");
+            else if ((y[1] >= y[0]) && (y[1] >= y[2])) MessageBox.Show("Wybrana litera to G", "Wynik");
+            else if ((y[2] >= y[0]) && (y[2] >= y[1])) MessageBox.Show("Wybrana litera to R", "Wynik");
+        }
+
+        private void button3_Click(object sender, EventArgs e) //wyczyszczenie zawartości obrazka
+        {
+            g.Clear(Color.White);
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)  //rysowanie czarnej linii na PictureBoxie
+        {
+            if (klik)
+            {
+                g.Clear(Color.White);
+                pictureBox1.Refresh();
+                klik = false;
+            }
+
+            if (e.Button == MouseButtons.Left)
+            {
+                maluj = true;
+                p = e.Location;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (maluj && e.Button == MouseButtons.Left)
+            {
+                g.DrawLine(rys, p, e.Location);
+                p = e.Location;
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            maluj = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Skalowanie();
+            pictureBox1.Image.Save(@"obrazyWzorcowe\(new)");
+        }
+    }
+}
